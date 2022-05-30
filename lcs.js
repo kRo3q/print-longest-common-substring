ffa26bfe42ab25461e324fcc9227576aab687030
kRo3q <alekspt1999@tlen.pl>
2022-05-30 20:01:20 +0200

    not working

--- lcs.js
+++ lcs.js
@@ -1,52 +1,37 @@
-function printLCSubStr(X,Y,m,n)
+var input = [];
+
+for (let i = 2; i <=process.argv.length; i++){
+    input[i-2] = process.argv[i];
+}
+
+
+
+function printLCSubStr(arr)
 {
+    var n = arr.length;
+    var s = arr[0];
+    var len = s.length;
+    var res = "";
 
-    let LCSuff = new Array(m+1);  
-    let len = 0;
-    let row = 0, col = 0;
-
-
-    for (let i = 0; i <= m; i++) {
-        LCSuff[i] = Array(n+1);
-        for (let j = 0; j <= n; j++) {
-            LCSuff[i][j]=0;   
-            if (i == 0 || j == 0)
-                LCSuff[i][j] = 0;
-
-            else if (X[i-1] == Y[j-1]) {
-                LCSuff[i][j] = LCSuff[i - 1][j - 1] + 1;
-                if (len < LCSuff[i][j]) {
-                    len = LCSuff[i][j];
-                    row = i;
-                    col = j;
+    for (let i = 0; i < len; i++) {
+        for (let j = i + 1; j <= len; j++) {
+            var stem = s.substring(i,j);
+            var k = 1;
+            for (k = 1; k < n; k++) {
+                if (!arr[k].includes(stem)) {
+                    break;
                 }
             }
-            else
-                LCSuff[i][j] = 0;
+        }
+        if (k===n && res.length < stem.length){
+            res = stem;
         }
     }
-
-    if (len == 0) {
-        console.log("\n");
-        return;
-    }
-
-    let resultStr = "";
-
-
-    while (LCSuff[row][col] != 0) {
-        resultStr = X[row-1] + resultStr;
-        --len;
-        row--;
-        col--;
-    }
-
-   console.log(resultStr);
+    return res;
 }
- 
 
-let X = process.argv[2];
-let Y = process.argv[3];
-let m = X.length;
-let n = Y.length;
-printLCSubStr(X, Y, m, n);
+
+
+console.log(printLCSubStr(input));
+
+
